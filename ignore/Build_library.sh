353bf9d17f77925de44fb7ee8ba6a4c674bf0dc4
rwcuffney <rwcuffney@gmail.com>
2023-08-15 12:27:26 -0400

    removing ignore directory

--- ignore/Build_library.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#!/bin/bash
-#source /Users/rwcuffney/.venv/LN_API_library_venv/bin/activate
-
-# Keep the terminal open
-#bash
-/Users/rwcuffney/.venv/LN_API_library_venv/Python_pkg/lexisnexisapi/python3 -m build
-/Users/rwcuffney/.venv/LN_API_library_venv/Python_pkg/lexisnexisapi/python3 -m twine upload dist/* 
